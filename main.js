330f8aec1345198ed608bb5958a9ae16f958f149
rpp2991 <renanppires29@gmail.com>
2023-02-14 17:01:36 -0300

    Arquivos inseridos

---
+++ main.js
@@ -0,0 +1,8 @@
+//Máscara para CPF e CEP e INCLUSÃO BÔNUS: Máscara de Telefone padrão Celular 
+$(document).ready(function () {
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
+    $('#telefone').mask('(00)00000-0000');
+});
+
+
